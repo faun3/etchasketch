3c97a8778cd141774045a69024977cc40d16e533
faun3 <antonioculcea@gmail.com>
2023-02-21 21:11:32 +0200

    create initial 16x16 grid of divs

--- javascript.js
+++ javascript.js
@@ -2,7 +2,8 @@
 let size = 16;
 const gridContainer = document.querySelector('.grid');
 const pixel = document.createElement('div');
-pixel.style.cssText = 'border: 2px; border-color: black';
+pixel.style.cssText = 'border: 2px; border-color: black; background-color: black;';
+pixel.textContent='a';
 for (let i = 1; i <= size * size; i++){
-    gridContainer.appendChild(pixel);
+    gridContainer.appendChild(pixel.cloneNode(true));
 }
