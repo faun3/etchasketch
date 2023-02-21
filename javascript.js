5cfaddf5e4a6851719ceba43932aa5c2a3b4554a
faun3 <antonioculcea@gmail.com>
2023-02-21 21:17:29 +0200

    add 64 divs without content and borders

--- javascript.js
+++ javascript.js
@@ -2,8 +2,7 @@
 let size = 16;
 const gridContainer = document.querySelector('.grid');
 const pixel = document.createElement('div');
-pixel.style.cssText = 'border: 2px; border-color: black; background-color: black;';
-pixel.textContent='a';
+pixel.style.cssText = 'border: 1px solid black;';
 for (let i = 1; i <= size * size; i++){
     gridContainer.appendChild(pixel.cloneNode(true));
 }
