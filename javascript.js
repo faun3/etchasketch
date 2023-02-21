67f720d933a20e3774c8762508c68c08d507ac33
faun3 <antonioculcea@gmail.com>
2023-02-21 22:17:04 +0200

    try percentage based hard-coded layout

--- javascript.js
+++ javascript.js
@@ -3,6 +3,9 @@ let size = 16;
 const gridContainer = document.querySelector('.grid');
 const pixel = document.createElement('div');
 pixel.style.cssText = 'border: 1px solid black;';
+pixel.style.cssText = 'width: 6.25%; margin: auto;';
+pixel.style.cssText = 'aspect-ratio: 1 / 1;';
+pixel.textContent = 'ab';
 for (let i = 1; i <= size * size; i++){
     gridContainer.appendChild(pixel.cloneNode(true));
 }
