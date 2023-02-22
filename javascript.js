46f397e730bdf0e7884d5f36011c4863ff2bf2ac
faun3 <antonioculcea@gmail.com>
2023-02-22 22:50:10 +0200

    add eventlistener to squares

--- javascript.js
+++ javascript.js
@@ -2,9 +2,29 @@
 let size = 10;
 const gridContainer = document.querySelector('.grid');
 const pixel = document.createElement('div');
-pixel.textContent = 'ab';
 pixel.style = 'aspect-ratio: 1;';
 gridContainer.style = `grid-template-columns: repeat(${size}, 1fr);`
 for (let i = 1; i <= size * size; i++){
     gridContainer.appendChild(pixel.cloneNode(true));
 }
+
+pixel.addEventListener('onmouseover', function (e) {
+    console.log(e);
+});
+
+const squares = document.querySelectorAll('.grid > div');
+squares.forEach((square) => {
+    square.addEventListener('onmouseover', () => {
+        square.style = 'background-color: black;';
+    });
+
+    square.addEventListener('onmouseout', () => {
+        square.style = 'background-color: white;';
+    });
+});
+//const pixels = document.querySelectorAll(pixel);
+//pixels.forEach((pixel) => {
+    //pixel.addEventListener('onmousehover', function (e) => {
+        //console.log(e);
+    //});
+//});
