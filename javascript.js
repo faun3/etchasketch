ac3ca726280f9a5df646286af3a9269c2065f08e
faun3 <antonioculcea@gmail.com>
2023-02-22 22:13:35 +0200

    make divs into dynamic squares

--- javascript.js
+++ javascript.js
@@ -3,6 +3,7 @@ let size = 16;
 const gridContainer = document.querySelector('.grid');
 const pixel = document.createElement('div');
 pixel.textContent = 'ab';
+pixel.style = 'aspect-ratio: 1;';
 for (let i = 1; i <= size * size; i++){
     gridContainer.appendChild(pixel.cloneNode(true));
 }
