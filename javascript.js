1378b631b80dcda76f5740e56313e9be71ef8067
faun3 <antonioculcea@gmail.com>
2023-03-03 15:29:11 +0200

    fix drawings staying on screen after resize

--- javascript.js
+++ javascript.js
@@ -5,6 +5,8 @@ function createBoard(size)
         return;
     } 
     const board = document.querySelector('.board');
+    const squares = board.querySelectorAll('div');
+    squares.forEach(div => div.remove());
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
